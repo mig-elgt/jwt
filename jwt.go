d7c10d12849a55e772d7572e358e9402994fa498
Miguel Galicia <migel.gt@outlook.com>
2023-01-11 10:39:09 -0600

    change customm payload as generic data

--- jwt.go
+++ jwt.go
@@ -9,8 +9,8 @@ import (
 
 // JWTCreator represents the JWT behavior.
 type TokenCreateValidator interface {
-	Create(id int64, role string) (string, error)
-	Validate(string) (*PayloadContent, error)
+	Create(payload interface{}) (string, error)
+	Validate(string) (interface{}, error)
 }
 
 // jwt represents a Token implementation of JWTCreator interface.
@@ -20,7 +20,7 @@ type jwt struct {
 }
 
 // New creates new JWT object and set its secret key.
-func New(secret string) *jwt {
+func New(secret string) TokenCreateValidator {
 	return &jwt{
 		secret: secret,
 		// Default expires time to 1 month
@@ -28,11 +28,27 @@ func New(secret string) *jwt {
 	}
 }
 
-func NewWithExpiresAt(secret string, expiresAt int) *jwt {
+// NewWithExpiresAt creates new jwt with expires time
+func NewWithExpiresAt(secret string, expiresAt int) TokenCreateValidator {
 	return &jwt{
 		secret:    secret,
 		expiresAt: time.Hour * time.Duration(expiresAt),
 	}
+}
+
+// Create creates, signs, and encodes a JWT token using the HMAC signing method
+func (t jwt) Create(data interface{}) (string, error) {
+	// create a signer for HS256
+	token := jwtgo.New(jwtgo.GetSigningMethod("HS256"))
+	// set our claims
+	token.Claims = &customClaimsGenericData{
+		data,
+		jwtgo.StandardClaims{
+			ExpiresAt: time.Now().Add(t.expiresAt).Unix(),
+		},
+	}
+	// create token string
+	return token.SignedString([]byte(t.secret))
 }
 
 // customClaims represents the payload information about the user
@@ -44,36 +60,56 @@ type customClaims struct {
 	jwtgo.StandardClaims
 }
 
-// Create creates, signs, and encodes a JWT token using the HMAC signing method
-func (t jwt) Create(id int64, role string) (string, error) {
-	// create a signer for HS256
-	token := jwtgo.New(jwtgo.GetSigningMethod("HS256"))
-	// set our claims
-	token.Claims = &customClaims{
-		id,
-		role,
-		jwtgo.StandardClaims{
-			ExpiresAt: time.Now().Add(t.expiresAt).Unix(),
-		},
-	}
-	// create token string
-	return token.SignedString([]byte(t.secret))
+// // Create creates, signs, and encodes a JWT token using the HMAC signing method
+// func (t jwt) Create(id int64, role string) (string, error) {
+// 	// create a signer for HS256
+// 	token := jwtgo.New(jwtgo.GetSigningMethod("HS256"))
+// 	// set our claims
+// 	token.Claims = &customClaims{
+// 		id,
+// 		role,
+// 		jwtgo.StandardClaims{
+// 			ExpiresAt: time.Now().Add(t.expiresAt).Unix(),
+// 		},
+// 	}
+// 	// create token string
+// 	return token.SignedString([]byte(t.secret))
+// }
+
+// customClaims represents the payload information about the user
+// to save into JWT token.
+type customClaimsGenericData struct {
+	Data interface{} `json:"data"`
+	jwtgo.StandardClaims
 }
 
-type PayloadContent struct {
-	ID   int64
-	Role string
-}
+// type PayloadContent struct {
+// 	ID   int64
+// 	Role string
+// }
+
+// // Validate validates tokens using a secret key and custom claim.
+// // it returns an error in case the token is malformed or expired.
+// func (t jwt) Validate(tokenString string) (*PayloadContent, error) {
+// 	token, err := jwtgo.ParseWithClaims(tokenString, &customClaims{}, func(token *jwtgo.Token) (interface{}, error) {
+// 		return []byte(t.secret), nil
+// 	})
+// 	if err != nil {
+// 		return nil, errors.Wrap(err, "failed parse with claims")
+// 	}
+// 	claims, _ := token.Claims.(*customClaims)
+// 	return &PayloadContent{claims.ID, claims.Role}, nil
+// }
 
 // Validate validates tokens using a secret key and custom claim.
 // it returns an error in case the token is malformed or expired.
-func (t jwt) Validate(tokenString string) (*PayloadContent, error) {
-	token, err := jwtgo.ParseWithClaims(tokenString, &customClaims{}, func(token *jwtgo.Token) (interface{}, error) {
+func (t jwt) Validate(tokenString string) (interface{}, error) {
+	token, err := jwtgo.ParseWithClaims(tokenString, &customClaimsGenericData{}, func(token *jwtgo.Token) (interface{}, error) {
 		return []byte(t.secret), nil
 	})
 	if err != nil {
 		return nil, errors.Wrap(err, "failed parse with claims")
 	}
-	claims, _ := token.Claims.(*customClaims)
-	return &PayloadContent{claims.ID, claims.Role}, nil
+	claims, _ := token.Claims.(*customClaimsGenericData)
+	return claims.Data, nil
 }
--- jwt_test.go
+++ jwt_test.go
@@ -1,59 +1,82 @@
 package jwt
 
 import (
-	"fmt"
 	"reflect"
-	"strings"
 	"testing"
 	"time"
 
 	jwtgo "github.com/dgrijalva/jwt-go"
 )
 
-func TestNew(t *testing.T) {
-	for _, tc := range []struct {
-		name  string
-		input int64
-		role  string
-	}{
-		{
-			name:  "should has 3 components: header, payload, signature",
-			input: 1,
-			role:  "admin",
-		},
-	} {
-		t.Run(fmt.Sprintf("[case] %s", tc.name), func(t *testing.T) {
-			token, _ := New("secret").Create(tc.input, tc.role)
-			got := len(strings.Split(string(token), "."))
-			if got != 3 {
-				t.Errorf("got %v; want 3", got)
-			}
-		})
-	}
-}
+// func TestNew(t *testing.T) {
+// 	for _, tc := range []struct {
+// 		name  string
+// 		input int64
+// 		role  string
+// 	}{
+// 		{
+// 			name:  "should has 3 components: header, payload, signature",
+// 			input: 1,
+// 			role:  "admin",
+// 		},
+// 	} {
+// 		t.Run(fmt.Sprintf("[case] %s", tc.name), func(t *testing.T) {
+// 			token, _ := New("secret").Create(tc.input, tc.role)
+// 			got := len(strings.Split(string(token), "."))
+// 			if got != 3 {
+// 				t.Errorf("got %v; want 3", got)
+// 			}
+// 		})
+// 	}
+// }
+
+// func TestJWT_Create(t *testing.T) {
+// 	type payload struct {
+// 		id   int
+// 		role string
+// 	}
+// 	for _, tc := range []struct {
+// 		name string
+// 		data *payload
+// 	}{
+// 		{
+// 			name: "should has 3 components: header, payload, signature",
+// 			data: &payload{id: 1, role: "admin"},
+// 		},
+// 	} {
+// 		t.Run(fmt.Sprintf("[case] %s", tc.name), func(t *testing.T) {
+// 			token, _ := New("secret").Create(tc.data)
+// 			got := len(strings.Split(string(token), "."))
+// 			if got != 3 {
+// 				t.Errorf("got %v; want 3", got)
+// 			}
+// 		})
+// 	}
+// }
 
 func TestJWT_Validate(t *testing.T) {
+	type payload struct {
+		ID   int    `json:"id"`
+		Role string `json:"role"`
+	}
 	type args struct {
-		id     int64
-		role   string
-		create func(id int64, role string) string
+		data   *payload
+		create func(data interface{}) (string, error)
 	}
 	cases := []struct {
 		name        string
 		args        args
-		wantPayload *PayloadContent
+		wantPayload map[string]interface{}
 		wantErr     bool
 	}{
 		{
 			name: "signature incorrect",
 			args: args{
-				id:   1,
-				role: "admin",
-				create: func(id int64, role string) string {
+				data: &payload{ID: 1, Role: "admin"},
+				create: func(data interface{}) (string, error) {
 					token := jwtgo.New(jwtgo.GetSigningMethod("HS256"))
-					token.Claims = &customClaims{id, role, jwtgo.StandardClaims{}}
-					t, _ := token.SignedString([]byte("fake_secret"))
-					return t
+					token.Claims = &customClaimsGenericData{data, jwtgo.StandardClaims{}}
+					return token.SignedString([]byte("fake_secret"))
 				},
 			},
 			wantErr: true,
@@ -61,26 +84,30 @@ func TestJWT_Validate(t *testing.T) {
 		{
 			name: "base case",
 			args: args{
-				id:   10,
-				role: "admin",
-				create: func(id int64, role string) string {
+				data: &payload{ID: 10, Role: "admin"},
+				create: func(data interface{}) (string, error) {
 					token := jwtgo.New(jwtgo.GetSigningMethod("HS256"))
-					token.Claims = &customClaims{id, role, jwtgo.StandardClaims{}}
-					t, _ := token.SignedString([]byte("secret"))
-					return t
+					token.Claims = &customClaimsGenericData{data, jwtgo.StandardClaims{}}
+					return token.SignedString([]byte("secret"))
 				},
 			},
-			wantErr:     false,
-			wantPayload: &PayloadContent{10, "admin"},
+			wantErr: false,
+			wantPayload: map[string]interface{}{
+				"id":   float64(10),
+				"role": "admin",
+			},
 		},
 	}
 	for _, tc := range cases {
 		t.Run(tc.name, func(t *testing.T) {
-			jsonWebToken := jwt{secret: "secret", expiresAt: time.Second * 10}
-			if pl, err := jsonWebToken.Validate(tc.args.create(tc.args.id, tc.args.role)); (err != nil) != tc.wantErr {
-				t.Errorf("jwt.Validate() error got: %v; want %v", err, tc.wantErr)
-			} else if !tc.wantErr && !reflect.DeepEqual(pl, tc.wantPayload) {
-				t.Errorf("jwt.Validate() Payload got: %v; want %v", pl, tc.wantPayload)
+			toker := jwt{secret: "secret", expiresAt: time.Second * 10}
+			token, _ := tc.args.create(tc.args.data)
+			pl, err := toker.Validate(token)
+			if (err != nil) != tc.wantErr {
+				t.Fatalf("jwt.Validate() got error: %v; want %v", err, tc.wantErr)
+			}
+			if !tc.wantErr && !reflect.DeepEqual(pl, tc.wantPayload) {
+				t.Fatalf("jwt.Validate() got payload: %v; want %v", pl, tc.wantPayload)
 			}
 		})
 	}
